9e316ebafb10111eeac0bc1d21542ecf68598b00
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-09-17 12:07:29 +0530

    import java.util.*; class file {     public static void main(String agrs[])     {         Scanner k = new Scanner(System.in);         double a;         a=k.nextDouble();         double s=3.14*a*a;         System.out.format("%.2f",s);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class file
+{
+    public static void main(String agrs[])
+    {
+        Scanner k = new Scanner(System.in);
+        double a;
+        a=k.nextDouble();
+        double s=3.14*a*a;
+        System.out.format("%.2f",s);
+    }
+}
