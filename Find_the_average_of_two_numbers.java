3241b59064e169ddab3e976d10711594793b1204
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-09-19 23:40:33 +0530

    import java.util.*; public class file {     public static void main(String args[])     {         Scanner k=new Scanner(System.in);         float a,b,c;         a=k.nextFloat();         b=k.nextFloat();         c=(a+b)/2;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class file
+{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+        float a,b,c;
+        a=k.nextFloat();
+        b=k.nextFloat();
+        c=(a+b)/2;
+        System.out.printf("%.4f",c);
+    }
+}
