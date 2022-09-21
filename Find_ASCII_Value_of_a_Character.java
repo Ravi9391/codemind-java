8334a361c825a0470ac22f6dd85007c872523386
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-09-21 23:44:47 +0530

    import java.util.*; public class file {     public static void main(String args[])     {         Scanner k=new Scanner(System.in);         char ch=k.next().charAt(0);         int a=ch;         System.out.println(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class file
+{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+        char ch=k.next().charAt(0);
+        int a=ch;
+        System.out.println(a);
+    }
+}
