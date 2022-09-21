6b9de7cbe5331fba7e570bb57b8f69df8c44c020
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-09-21 23:53:27 +0530

    import java.util.*; public class file {     public static void main(String args[])     {         Scanner k=new Scanner(System.in);         int a,b,c;         a=k.nextInt();         b=k.nextInt();         c=(a*b);         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class file
+{
+    public static void main(String args[])
+    {
+        Scanner k=new Scanner(System.in);
+        int a,b,c;
+        a=k.nextInt();
+        b=k.nextInt();
+        c=(a*b);
+        System.out.println(c);
+    }
+}
