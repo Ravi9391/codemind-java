adcaa48a8d16ea37b6d7b6166a08b89f9d6f218b
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-10-04 00:40:33 +0530

    import java.util.*; class file {     public static void main(String ravi[])     {         Scanner d=new Scanner(System.in);         int r;         r=d.nextInt();         if(r%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class file
+{
+    public static void main(String ravi[])
+    {
+        Scanner d=new Scanner(System.in);
+        int r;
+        r=d.nextInt();
+        if(r%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
