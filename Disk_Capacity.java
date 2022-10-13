c138576195f214b099ba4cc18572ee709501db64
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-10-13 12:12:03 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner ab=new Scanner(System.in);         int a,b,c;         a=ab.nextInt();         b=ab.nextInt();         c=ab.nextInt();         System.out.println(a*b*c*1024);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner ab=new Scanner(System.in);
+        int a,b,c;
+        a=ab.nextInt();
+        b=ab.nextInt();
+        c=ab.nextInt();
+        System.out.println(a*b*c*1024);
+    }
+}
