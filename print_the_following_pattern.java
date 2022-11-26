0f50388c397dc5549001954a5ef2b60f49e3956f
Ravi9391 <96769680+Ravi9391@users.noreply.github.com>
2022-11-26 10:33:07 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=i;j++)             {                 if(j==1 ||i==j ||i==n)                 {                     System.out.print("*");                 }                 else                 {                     System.out.print(" ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -5,11 +5,18 @@ class Solution
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=n;i>=1;i--)
+        for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.printf("%c ",i+64);
+                if(j==1 ||i==j ||i==n)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
